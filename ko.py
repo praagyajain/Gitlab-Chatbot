48d6f362abcc389ea9411ca258a05bb1f66387ba
praagyajain <praagyajain2002@gmail.com>
2026-03-17 21:40:09 +0530

    updated gitignore
    Signed-off-by: praagyajain <praagyajain2002@gmail.com>

--- ko.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyA5v72cFKYlWw9dz6rfMVrrYfh9EMLKhmI")
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)# cook your dish here
